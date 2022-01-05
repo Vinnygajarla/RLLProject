a4bba70dd0d6c77f323ecb23fa6f6099734d617b
Vinnygajarla <vineelagajarla27@gmail.com>
2022-01-05 18:33:27 +0530

    rll project

---
+++ SpringHelloWorld/src/main/java/com/java/spr/HelloImpl.java
@@ -0,0 +1,23 @@
+package com.java.spr;
+
+public class HelloImpl implements Hello  {
+
+		String greeting;
+		
+
+		public String getGreeting() {
+			return greeting;
+		}
+
+
+		public void setGreeting(String greeting) {
+			this.greeting = greeting;
+		}
+
+
+		@Override
+		public String sayHello(String name) {
+			// TODO Auto-generated method stub
+			return greeting+name;
+		}
+}
